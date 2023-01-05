d3f90382bbd2a5e91a587c4ecfe52a0994f1638f
Joseph DiMino <59896424+DiMino-0@users.noreply.github.com>
2023-01-05 18:47:21 -0500

    First commit

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,94 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+    /*
+     * Vendor deps:
+     * Rev: https://software-metadata.revrobotics.com/REVLib.json
+     * NavX: https://www.kauailabs.com/dist/frc/2022/navx_frc.json
+     */
+    // Robot stats
+    public static final double LENGTH_TO_WHEELS = 0.381;
+    public static final double WIDTH_TO_WHEELS = 0.381;
+
+    public static final double kEncoderResolution = 414.1666667;// 596.4
+
+    public static final double GEAR_RATIO = 6.67;
+    public static final double CIRCUMFERENCE = 4 * Math.PI;
+    public static final double MAX_VELOCITY = 5; // units: m/s
+
+    // CAN ID
+    public static final int FRONT_LEFT_CAN = 1;
+    public static final int FRONT_RIGHT_CAN = 2;
+    public static final int BACK_RIGHT_CAN = 4;
+    public static final int BACK_LEFT_CAN = 3;
+
+    // PWM
+    public static final int FRONT_LEFT = 1;
+    public static final int FRONT_RIGHT = 2;
+    public static final int BACK_RIGHT = 3;
+    public static final int BACK_LEFT = 4;
+
+    // ENCODERS
+    public static final int FRONT_LEFT_PORT_1 = 0;
+    public static final int FRONT_LEFT_PORT_2 = 1;
+
+    public static final int FRONT_RIGHT_PORT_1 = 2;
+    public static final int FRONT_RIGHT_PORT_2 = 3;
+
+    public static final int BACK_RIGHT_PORT_1 = 8;
+    public static final int BACK_RIGHT_PORT_2 = 9;
+
+    public static final int BACK_LEFT_PORT_1 = 6;
+    public static final int BACK_LEFT_PORT_2 = 7;
+
+    public static final double kMaxSpeedMetersPerSecond = 3;
+    public static final double kMaxAccelerationMetersPerSecondSquared = 3;
+    public static final double kMaxAngularSpeedRadiansPerSecond = Math.PI;
+    public static final double kMaxAngularSpeedRadiansPerSecondSquared = Math.PI;
+    // Distance between centers of right and left wheels on robot
+    public static final double kTrackWidth = 0.41;
+    // Distance between front and back wheels on robot
+
+    public static final double kWheelBase = 0.66;
+    public static final SwerveDriveKinematics m_kinematics = new SwerveDriveKinematics(
+            new Translation2d(kWheelBase / 2, kTrackWidth / 2),
+            new Translation2d(kWheelBase / 2, -kTrackWidth / 2),
+            new Translation2d(-kWheelBase / 2, kTrackWidth / 2),
+            new Translation2d(-kWheelBase / 2, -kTrackWidth / 2));
+
+    public static final TrapezoidProfile.Constraints kThetaControllerConstraints = new TrapezoidProfile.Constraints(
+            kMaxAngularSpeedRadiansPerSecond, kMaxAngularSpeedRadiansPerSecondSquared);
+    public static final double kpDriveVel = 2.7066;
+
+    public static final class OIConstants {
+        public static final int kDriverControllerPort = 1;
+
+        public static final int kDriverYAxis = 1;
+        public static final int kDriverXAxis = 0;
+        public static final int kDriverRotAxis = 2;
+        public static final int kDriverFieldOrientedButtonIdx = 1;
+
+        public static final double kDeadband = 0.05;
+    }
+
+}
---
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -0,0 +1,127 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.RelativeEncoder;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import frc.robot.Constants;
+import java.lang.Math;
+
+/** Add your docs here. */
+public class SwerveModule {
+    private int tmp;
+
+    private Spark turningMotor;
+    private CANSparkMax driveMotor;
+
+    // private Encoder turningEncoder;
+    private Encoder turningEncoder;
+    private RelativeEncoder driveEncoder;
+
+    private PIDController turningPidController;
+
+    public SwerveModule(Drivetrain d, int driveChannel, int turnChannel, int channel1, int channel2,
+            boolean driveMotorReversed, boolean turningMotorReversed) {
+        tmp = 0;
+        turningMotor = new Spark(turnChannel);
+        driveMotor = new CANSparkMax(driveChannel, MotorType.kBrushless);
+
+        turningMotor.setInverted(turningMotorReversed);
+        driveMotor.setInverted(driveMotorReversed);
+
+        resetMotors();
+
+        turningEncoder = new Encoder(channel2, channel1);
+        driveEncoder = driveMotor.getEncoder();
+
+        driveEncoder.setPositionConversionFactor(
+                Units.inchesToMeters(Constants.CIRCUMFERENCE) / (60.0 * Constants.GEAR_RATIO));
+        // driveEncoder.setVelocityConversionFactor();
+        // turningEncoder.setDistancePerPulse(360 / Constants.kEncoderResolution);
+        turningEncoder.setDistancePerPulse(Math.PI * 2 / Constants.kEncoderResolution);
+
+        turningPidController = new PIDController(0.4, 0, 0);
+        // SASHA'S NOTES: encoder distance will be in radians
+        turningPidController.setTolerance(Math.PI * 5 / 180);
+        turningPidController.enableContinuousInput(-Math.PI, Math.PI);
+
+        resetEncoders();
+    }
+
+    public double getDrivePosition() {
+        return driveEncoder.getPosition();
+    }
+
+    public double getDriveVelocity() {
+        return driveEncoder.getVelocity();
+    }
+
+    public double getTurningPosition() {
+        double pos = turningEncoder.getDistance();
+        // if(pos < -1 * Math.PI)
+        // {
+        //     while(pos < -1 * Math.PI)
+        //     {
+        //         pos += Math.PI;
+        //     }
+        // }
+        // else if(pos > Math.PI)
+        // {
+        //     while(pos > Math.PI)
+        //     {
+        //         pos -= Math.PI;
+        //     }
+        // }
+        return pos;//Math.IEEERemainder(turningEncoder.getDistance(), Math.PI);
+    }
+
+    public SwerveModuleState getState() {
+        return new SwerveModuleState(getDriveVelocity(), new Rotation2d(getTurningPosition()));
+    }
+
+    public void setDesiredState(SwerveModuleState state) {
+        //state = SwerveModuleState.optimize(state, getState().angle);
+        tmp = (tmp + 1) % 100;
+        double speed = turningPidController.calculate(getTurningPosition(), state.angle.getRadians());
+        if(tmp % 100 == 0)
+        {
+            System.out.println("SPEED: " + state.speedMetersPerSecond);
+            System.out.println("CURR: " + getTurningPosition());
+            System.out.println("Target: " + state.angle.getRadians());
+            System.out.println("CALC_speed: " + speed);
+            System.out.println("emcpder: " + turningEncoder.getDistance());
+        }
+        driveMotor.set(state.speedMetersPerSecond);
+        turningMotor.set(speed);
+    }
+
+    public void stop() {
+        driveMotor.set(0);
+        turningMotor.set(0);
+    }
+
+    public Rotation2d getAngle()
+    {
+        return getState().angle;
+    }
+
+    private void resetEncoders() {
+        turningEncoder.reset();
+        driveEncoder.setPosition(0);
+    }
+
+    private void resetMotors() {
+        driveMotor.restoreFactoryDefaults();
+        turningMotor.setSafetyEnabled(false);
+    }
+}
